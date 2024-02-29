7b68e516e283e8f0d0050346831a76024f5560db
Park Hyeongmin <thalsal@naver.com>
2024-02-29 20:24:15 +0900

    Title: 자리 바꾸기 2, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240229/자리 바꾸기 2/changing-seats-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println("4");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("1");
+
+    }
+}
