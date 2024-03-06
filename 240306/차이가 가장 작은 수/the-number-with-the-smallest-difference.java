f10860e29b026b071a36475ef02e22645c7743e9
Park Hyeongmin <thalsal@naver.com>
2024-03-06 17:29:45 +0900

    Title: 차이가 가장 작은 수, Time: 3092ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240306/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long m = sc.nextLong();
+
+        TreeSet<Long> treeSet = new TreeSet<>();
+
+        long result = Integer.MAX_VALUE;
+
+        while (n-- > 0) {
+            treeSet.add(sc.nextLong());
+        }
+
+        Long biggest = treeSet.last();
+        Long lower = treeSet.lower(biggest);
+
+        while (!treeSet.isEmpty()) {
+
+            if (lower == null) {
+                break;
+            }
+
+            long mathResult = biggest - lower;
+            if (mathResult >= m) {
+                result = Math.min(result, mathResult);
+
+                biggest = treeSet.lower(treeSet.last());
+                treeSet.remove(treeSet.last());
+                lower = treeSet.lower(biggest);
+            }
+
+            if (mathResult < m) {
+                lower = treeSet.lower(lower);
+            }
+        }
+
+        System.out.println(result==Integer.MAX_VALUE?-1 : result);
+
+    }
+}
